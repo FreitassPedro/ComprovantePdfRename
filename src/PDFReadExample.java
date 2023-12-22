f02caf625a8ece04575ee223593db4055989022b
PedroOWN <pedrospf2018@gmail.com>
2023-12-22 15:59:23 -0300

    Arrumado Arquivos renomeados nome duplicado

--- src/PDFReadExample.java
+++ src/PDFReadExample.java
@@ -6,19 +6,21 @@ Como forma de estudos
 Programa para renomear arquivos (comprovantes pdf) banco Sicredi
 ****************************************************************
  */
+import static Types.fileInfos.criadorRepositorio;
 import static Types.fileInfos.renameFiles;
 
 public class PDFReadExample {
     public static void main(String[] args) {
+        mensagemBoasVindas();
+        criadorRepositorio();
         renameFiles();
     }
 
     private static void mensagemBoasVindas() {
         System.out.println("Siga as intruções:");
         System.out.println("""
-                Para começar, coloque todos arquivos de comprovante PDF na pasta PIX na área de trabalho!
-                Se não tiver, basta apenas criar!
-                Vamos começar!
+                Para começar, coloque todos arquivos de comprovante PDF na pasta COMPROVANTES na área de trabalho!
+                Se não tiver, criamos agora!
                 """);
     }
 }
--- src/Types/fileInfos.java
+++ src/Types/fileInfos.java
@@ -3,17 +3,26 @@ package Types;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static Types.Types.*;
 
 public class fileInfos {
     public static String directory() {
         String username = System.getProperty("user.name");
-        return "C:\\Users\\" + username + "\\Desktop\\docError\\teste\\";
-        // return "C:\\Users\\" + username + "\\Desktop\\projetoExcel\\comprovantes\\";
+        return "C:\\Users\\" + username + "\\Desktop\\Comprovantes\\";
     }
+
+    private static String directoryRenomeados() {
+        String username = System.getProperty("user.name");
+        return "C:\\Users\\" + username + "\\Desktop\\Comprovantes\\Renomeados\\";
+    }
+
     public static String fileText(String path) throws IOException {
         try (PDDocument document = PDDocument.load(new File(path))) {
             if (!document.isEncrypted()) {
@@ -37,62 +46,35 @@ public class fileInfos {
             return 2;
         }
     }
-    private static final String novoNome = "comprovante";
+
     public static void renameFiles() {
         File diretorio = new File(directory());
-            File[] arquivos = diretorio.listFiles();
-        int contadorComprovante = 0;
+        File[] arquivos = diretorio.listFiles();
+        short contadorComprovante = 0;
         if (arquivos != null) {
             for (int i = 0; i < arquivos.length; i++) {
                 File arquivo = arquivos[i];
                 if (arquivo.isFile()) {
-                    String novoNomeArquivo = novoNome + " " + i + ".pdf";
-                    File novoArquivo = new File(arquivo.getParent(), novoNomeArquivo);
+                    String nomeTemporario = "comprovante " + (i + 1) + ".pdf";
+                    File fileTemporario = new File(arquivo.getParent(), nomeTemporario);
 
-                    int contador = 2;
-                    //TRANSFORMA NOME DO ARQUIVO EM "COMPROVANTE (i)"
-                    while (novoArquivo.exists()) {
-                        String nomeSemExtensao = novoNomeArquivo.substring(0, novoNomeArquivo.lastIndexOf('.'));
-                        String extensao = novoNomeArquivo.substring(novoNomeArquivo.lastIndexOf('.'));
-                        novoNomeArquivo = nomeSemExtensao + " (" + contador + ")" + extensao;
-                        novoArquivo = new File(arquivo.getParent(), novoNomeArquivo);
-                        contador++;
-                    }
-
-                    boolean renomeadoComSucesso = arquivo.renameTo(novoArquivo);
+                    boolean renomeadoComSucesso = arquivo.renameTo(fileTemporario);
 
                     if (renomeadoComSucesso) {
-                        String nomeComInfos =  directory() + novoNomeArquivo;
-                        String novoNomeFinal = "";
+                        String nomeComInfos = directory() + nomeTemporario;
+                        String dadosExtraidos = "";
                         try {
-                            int fileType = fileInfos.fileType(novoArquivo.getPath());
-                            if (fileType == 1) {
-                                novoNomeFinal = pegarInfosTipo1(nomeComInfos);
-                            } else if (fileType == 2) {
-                                novoNomeFinal = pegarInfosTipo2(nomeComInfos);
-                            }
-                            else if (fileType == 3) {
-                                novoNomeFinal = pegarInfosTipo3(nomeComInfos);
+                            switch (fileInfos.fileType(fileTemporario.getPath())) {
+                                case 1 -> dadosExtraidos = pegarInfosTipo1(nomeComInfos);
+                                case 2 -> dadosExtraidos = pegarInfosTipo2(nomeComInfos);
+                                case 3 -> dadosExtraidos = pegarInfosTipo3(nomeComInfos);
                             }
 
-                            /*Renomeia o arquivo com a String novoNomeFinal
-                            Falha caso já existir algum arquivo com o mesmo nome, então adiciona "(2)"
-                            */
+                            renomeacaoFinal(fileTemporario, dadosExtraidos, nomeTemporario);
+                            contadorComprovante++;
 
-                            File arquivoFinal = new File(directory(), novoNomeFinal + ".pdf");
-
-                            if (arquivoFinal.exists()) {
-                                // Se o arquivo final já existe, adicione ' (2)' ao nome
-                                arquivoFinal = new File(directory(), novoNomeFinal + " (2).pdf");
-                            }
-                            if (novoArquivo.renameTo(arquivoFinal)) {
-                                System.out.println("Arquivo " + novoNomeArquivo + " renomeado com sucesso para " + novoNomeFinal);
-                                contadorComprovante++;
-                            } else {
-                                System.out.println("Erro ao renomear o arquivo " + novoNomeArquivo);
-                            }
                         } catch (Exception e) {
-                            System.out.println("Algo deu errado" + novoNomeArquivo + ": " + e.getMessage());
+                            System.out.println("Algo deu errado" + nomeTemporario + ": " + e.getMessage());
                         }
                     } else {
                         System.out.println("Falha ao renomear o arquivo " + arquivo.getName());
@@ -101,6 +83,62 @@ public class fileInfos {
             }
         }
         System.out.println();
-        System.out.println(contadorComprovante + " Comprovantes foram renomeados! Operação concluída.");
+        abrirDiretorio(contadorComprovante);
+    }
+
+    //CLASSE QUE RENOMEIA O ARQUIVO COM AS INFORMAÇÕES EXTRAIDAS
+    private static void renomeacaoFinal(File fileTemporario, String dadosExtraidos, String nomeTemporario) {
+        File arquivoFinal = new File(directoryRenomeados(), dadosExtraidos + ".pdf");
+
+        if (arquivoFinal.exists()) {
+            // Se o arquivo final já existe, adicione ' (2)' ao nome
+            arquivoFinal = new File(directoryRenomeados(), dadosExtraidos + " (2).pdf");
+        }
+        if (fileTemporario.renameTo(arquivoFinal)) {
+            System.out.println("Arquivo " + nomeTemporario + " renomeado com sucesso para " + dadosExtraidos);
+        } else {
+            System.out.println("Erro ao renomear o arquivo " + nomeTemporario);
+        }
+    }
+
+    //CRIA UM DIRETÓRIO NO WINDOWS DO USUÁRIO PARA SALVAR O TXT
+    public static void criadorRepositorio() {
+        Path directoryPath = Paths.get(directoryRenomeados());
+
+        // Verifica se o diretório já existe
+        if (Files.exists(directoryPath)) {
+            System.out.println("Estou pronto, preparando os motores...");
+            System.out.println("Vamos lá:");
+            System.out.println();
+        } else {
+            try {
+                // Cria o diretório
+                Files.createDirectories(directoryPath);
+                System.out.println("Diretório criado:  " + directoryPath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void abrirDiretorio(short contadorComprovante) {
+        // Verifica se o Desktop é suportado
+        if (Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                // Abre o Explorer na pasta especificada
+                if (contadorComprovante != 0) {
+                    System.out.println(contadorComprovante + " Comprovantes foram renomeados! Operação concluída.");
+                    desktop.open(new File(directoryRenomeados()));
+                } else {
+                    desktop.open(new File(directory()));
+                    System.out.println("Nenhum compovante foi encontrado. Por favor, adicione nesta pasta.");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Desktop não é suportado.");
+        }
     }
 }
